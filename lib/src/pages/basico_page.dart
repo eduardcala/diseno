11d0f8cfa3ee9ef9d198b78363b282e52c667c43
Eduard Cala <eduardcala@gmail.com>
2020-09-25 00:08:48 -0500

    133.Diseño básico - parte 3

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -9,46 +9,57 @@ class BasicoPage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: Column(
-        children: [
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
           
-          _crearImagen(),          
-          _crearTitulo(),
-          _crearAcciones(),
-          _crearTexto(),
-          _crearTexto(),
-          _crearTexto(),
-          _crearTexto()
+            _crearImagen(),          
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto()
 
-        ],
+          ],
+        ),
       )
     );
   }
 
   Widget _crearImagen() {
-    return Image(image: NetworkImage('https://guidetoiceland.imgix.net/402731/x/0/vestrahorn-stokksnes-as-a-landscape-photography-destination-1?auto=compress%2Cformat&ch=Width%2CDPR&dpr=1&ixlib=php-3.3.0&w=1300&s=cb255905aaf3cb467ff0efa1522e3199'));
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage('https://guidetoiceland.imgix.net/402731/x/0/vestrahorn-stokksnes-as-a-landscape-photography-destination-1?auto=compress%2Cformat&ch=Width%2CDPR&dpr=1&ixlib=php-3.3.0&w=1300&s=cb255905aaf3cb467ff0efa1522e3199'),
+        height: 200.0,
+        fit: BoxFit.cover,
+        ),
+    );
   }
 
   Widget _crearTitulo() {
-    return Container(
-            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Lago con un puente', style: estiloTitulo),
-                      SizedBox( height: 7.0 ),
-                      Text('Un lago en Alemania', style: estiloSubTitulo),
-                    ],
+    return SafeArea(
+      child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Lago con un puente', style: estiloTitulo),
+                        SizedBox( height: 7.0 ),
+                        Text('Un lago en Alemania', style: estiloSubTitulo),
+                      ],
+                    ),
                   ),
-                ),
-                Icon(Icons.star, color: Colors.red, size: 30.0),
-                Text('41', style: TextStyle(fontSize:20.0))
-              ],
+                  Icon(Icons.star, color: Colors.red, size: 30.0),
+                  Text('41', style: TextStyle(fontSize:20.0))
+                ],
+              ),
             ),
-          );
+    );
   }
 
   Widget _crearAcciones() {
@@ -78,11 +89,13 @@ class BasicoPage extends StatelessWidget {
   }
 
   Widget _crearTexto() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      child: Text(
-        'Commodo non dolore amet quis et duis anim laborum id. Sint in ut anim velit dolore. Irure incididunt dolore qui adipisicing est anim Lorem. Eiusmod minim elit ullamco incididunt adipisicing consectetur occaecat elit minim duis ullamco ea. Aliqua minim fugiat do laborum proident elit. Adipisicing reprehenderit et eu cillum Lorem qui tempor.',
-        textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: Text(
+          'Commodo non dolore amet quis et duis anim laborum id. Sint in ut anim velit dolore. Irure incididunt dolore qui adipisicing est anim Lorem. Eiusmod minim elit ullamco incididunt adipisicing consectetur occaecat elit minim duis ullamco ea. Aliqua minim fugiat do laborum proident elit. Adipisicing reprehenderit et eu cillum Lorem qui tempor.',
+          textAlign: TextAlign.justify,
+        ),
       ),
     );
   }
