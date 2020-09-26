0ccc57766cd8a7d84b8a45c034d4eef9f5bed8f5
Eduard Cala <eduardcala@gmail.com>
2020-09-26 00:27:19 -0500

    140. Diseño compuesto - Table y TableRow

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -13,7 +13,8 @@ class BotonesPage extends StatelessWidget {
           SingleChildScrollView(
             child: Column(
               children: [
-                _titulos()
+                _titulos(),
+                _botonesRedondeados()
               ],
             ),
           )
@@ -131,6 +132,59 @@ class BotonesPage extends StatelessWidget {
       )
     );
 
+  }
+
+  Widget _botonesRedondeados() {
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _crearBotonRedondeado(),
+            _crearBotonRedondeado()
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(),
+            _crearBotonRedondeado()
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(),
+            _crearBotonRedondeado()
+          ]
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(),
+            _crearBotonRedondeado()
+          ]
+        )
+      ],
+    );
+  }
+
+  Widget _crearBotonRedondeado() {
+    return Container(
+      height: 180.0,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(62, 66, 107, 0.7),
+        borderRadius: BorderRadius.circular(20.0)
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.pinkAccent,
+            radius: 30.0,
+            child: Icon(Icons.swap_calls, color: Colors.white, size: 30.0),
+          ),
+          Text('Cosa', style: TextStyle( color: Colors.pinkAccent ),)
+        ],
+      ),
+    );
   }
 
 }
