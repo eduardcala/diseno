db896e421a867d5b9350c87d06c02dc0e9f3f6fd
Eduard Cala <eduardcala@gmail.com>
2020-09-25 22:58:33 -0500

    137. Diseño compuesto - Fondo y Gradientes

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -16,14 +16,68 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Text('Pagina 1'),
+    return Stack(
+      children: [
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _textos() {
+
+    final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(height: 20.0,),
+          Text( '11°', style: estiloTexto ),
+          Text( 'Miércoles', style: estiloTexto ),
+          Expanded(child: Container()),
+          Icon( Icons.keyboard_arrow_down, size: 70.0, color: Colors.white )
+        ],
+      ),
     );
   }
 
   Widget _pagina2() {
-    return Center(
-      child: Text('Pagina 2'),
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+      child: Center(
+        child: RaisedButton(
+          shape: StadiumBorder(),
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+            child: Text('Bienvenidos', style: TextStyle(fontSize: 20.0),),
+          ),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 
